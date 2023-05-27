265e7604830e66241f034fa9d630a899bbd7a27e
Angshuman Sarmah <angshuman.sarmah@gmail.com>
2023-05-28 00:21:13 +0530

    initial setup of service (#1)

---
+++ src/api/socialMediaDetails.ts
@@ -0,0 +1,10 @@
+import { SocialMediaDetailsModel } from "../model/socialMediaModel";
+
+const so: SocialMediaDetailsModel = {
+    user_id: 'string',
+    twitter: {
+        userName: 'string',
+        followers: 'string',
+        reach: 'string'
+    }
+} 
